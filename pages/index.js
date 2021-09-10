9b9d996f620b2a041d52d29c658a4c86bb905a82
shirshandu <shirshandu@yahoo.com>
2021-09-10 18:21:29 +0530

    :sparkles: nextjs with tailwind
    nextjs with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,51 @@
-import Head from 'next/head'
+import { render } from "react-dom";
+import Head from "next/Head";
+import Link from "next/Link";
+import Layout from "../component/Layout";
 
-export default function Home() {
+export default function Home({ pokemon }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+    <div>
+      <Layout title="Pokdex">
+        <h1 className="text-4xl mb-8 text-center"> Pokdex</h1>
+        <ul>
+          {pokemon.map((pokeman, index) => (
+            <li key={index}>
+              <Link href={`/pokemon?id=${index + 1}`}>
+                <a className="border p-4 border-gray my-2 capitalize flex items-center text-lg bg-gray-200 rounded-md">
+                  <img
+                    className="w-20 h-20 mr-3"
+                    src={pokeman.image}
+                    alt={pokeman.name}
+                  />
+                  <span className="mr-2 font-bold">{index + 1}</span>
+                  {pokeman.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+    const { results } = await res.json();
+    const pokemon = results.map((res, index) => {
+      const paddedIndex = ("00" + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return {
+        ...res,
+        image
+      };
+    });
+    return {
+      props: { pokemon }
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
